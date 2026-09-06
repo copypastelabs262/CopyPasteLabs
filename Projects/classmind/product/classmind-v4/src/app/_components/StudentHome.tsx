88ac38b512128f10cb6ce9011ab7393bca0d537e
copypastelabs262 <copypastelabs262@gmail.com>
2026-09-06 17:24:26 +0530

    Auto-save: StudentHome.tsx

--- Projects/classmind/product/classmind-v4/src/app/_components/StudentHome.tsx
+++ Projects/classmind/product/classmind-v4/src/app/_components/StudentHome.tsx
@@ -3,6 +3,7 @@
 import { useState, type FormEvent } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { CARRIED_QUESTION_KEY } from "./AskWorkspace";
 import {
   Button,
   ButtonLink,
