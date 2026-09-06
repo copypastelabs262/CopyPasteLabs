86ee3ea11d76b5b44868e7a1153e84bb210fc404
copypastelabs262 <copypastelabs262@gmail.com>
2026-09-06 21:29:53 +0530

    Auto-save: _phase2-cap.mjs, AppNav.tsx

--- Projects/classmind/product/classmind-v4/src/app/_components/AppNav.tsx
+++ Projects/classmind/product/classmind-v4/src/app/_components/AppNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cx } from "./ui";
